4bd412406dd6ac496c7f38af4c209256ebe298ee
juan <jsburckhardt@gmail.com>
2018-04-18 23:42:00 +1000

    testing fluent command line

--- Program.cs
+++ Program.cs
@@ -1,29 +1,74 @@
-﻿using System;
+﻿using Fclp;
+using System;
 
 namespace EncryptStringSample
 {
+    public class ApplicationArguments
+    {
+        public int RecordId { get; set; }
+        public bool Silent { get; set; }
+        public string NewValue { get; set; }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter a password to use:");
-            string password = Console.ReadLine();
-            Console.WriteLine("Please enter a string to encrypt:");
-            string plaintext = Console.ReadLine();
-            Console.WriteLine("");
+            var p = new FluentCommandLineParser<ApplicationArguments>();
 
-            Console.WriteLine("Your encrypted string is:");
-            string encryptedstring = StringCipher.Encrypt(plaintext, password);
-            Console.WriteLine(encryptedstring);
-            Console.WriteLine("");
+            // specify which property the value will be assigned too.
+            p.Setup(arg => arg.RecordId)
+             .As('r', "record") // define the short and long option name
+             .Required(); // using the standard fluent Api to declare this Option as required.
 
-            Console.WriteLine("Your decrypted string is:");
-            string decryptedstring = StringCipher.Decrypt(encryptedstring, password);
-            Console.WriteLine(decryptedstring);
-            Console.WriteLine("");
+            p.Setup(arg => arg.NewValue)
+             .As('v', "value")
+             .Required();
 
-            Console.WriteLine("Press any key to exit...");
-            Console.ReadLine();
+            p.Setup(arg => arg.Silent)
+             .As('s', "silent")
+             .SetDefault(false); // use the standard fluent Api to define a default value if non is specified in the arguments
+
+            var result = p.Parse(args);
+            Console.WriteLine(p.Object.NewValue);
+
+            if (result.HasErrors == false)
+            {
+                //Console.WriteLine(result.NewValue)
+                // use the instantiated ApplicationArguments object from the Object property on the parser.
+                //application.Run(p.Object);
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //Console.WriteLine("Please enter a password to use:");
+            //string password = Console.ReadLine();
+            //Console.WriteLine("Please enter a string to encrypt:");
+            //string plaintext = Console.ReadLine();
+            //Console.WriteLine("");
+//
+            //Console.WriteLine("Your encrypted string is:");
+            //string encryptedstring = StringCipher.Encrypt(plaintext, password);
+            //Console.WriteLine(encryptedstring);
+            //Console.WriteLine("");
+//
+            //Console.WriteLine("Your decrypted string is:");
+            //string decryptedstring = StringCipher.Decrypt(encryptedstring, password);
+            //Console.WriteLine(decryptedstring);
+            //Console.WriteLine("");
+//
+            //Console.WriteLine("Press any key to exit...");
+            //Console.ReadLine();
         }
     }
 }
