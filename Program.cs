343154868eac4f0770bf095cf1729886130c79fc
juan <jsburckhardt@gmail.com>
2018-04-20 00:27:30 +1000

    included parameters and environment variable

--- Program.cs
+++ Program.cs
@@ -5,70 +5,66 @@ namespace EncryptStringSample
 {
     public class ApplicationArguments
     {
-        public int RecordId { get; set; }
-        public bool Silent { get; set; }
-        public string NewValue { get; set; }
+        public ActionType Action { get; set; }
+        public string EncryptionKey { get; set; }
+        public string StringValue { get; set; }
+
+    }
+
+    public enum ActionType
+    {
+        Encrypt,
+        Decrypt
     }
     class Program
     {
         static void Main(string[] args)
         {
             var p = new FluentCommandLineParser<ApplicationArguments>();
-
             // specify which property the value will be assigned too.
-            p.Setup(arg => arg.RecordId)
-             .As('r', "record") // define the short and long option name
+            p.Setup(arg => arg.Action)
+             .As('a', "action") // define the short and long option name
              .Required(); // using the standard fluent Api to declare this Option as required.
-
-            p.Setup(arg => arg.NewValue)
-             .As('v', "value")
-             .Required();
-
-            p.Setup(arg => arg.Silent)
-             .As('s', "silent")
-             .SetDefault(false); // use the standard fluent Api to define a default value if non is specified in the arguments
+            p.Setup(arg => arg.StringValue)
+              .As('v', "stringvalue")
+              .Required();
+            p.Setup(arg => arg.EncryptionKey)
+              .As('k', "encryptionkey");
 
             var result = p.Parse(args);
-            Console.WriteLine(p.Object.NewValue);
-
+            var x =p.Object.Action.ToString();
+            
             if (result.HasErrors == false)
             {
-                //Console.WriteLine(result.NewValue)
-                // use the instantiated ApplicationArguments object from the Object property on the parser.
-                //application.Run(p.Object);
+                string password = p.Object.EncryptionKey;
+                if (string.IsNullOrEmpty(password))
+                {
+                    if (string.IsNullOrEmpty(Environment.GetEnvironmentVariable("ENCRYPTION_KEY")))
+                    {
+                        Console.WriteLine("There is no encryption key in the arguments neither environment variable ENCRYPTION_KEY");
+                        return;
+                    }
+                    password = Environment.GetEnvironmentVariable("ENCRYPTION_KEY");
+                    Console.WriteLine("Using environment variable to encrypt");
+                }
+
+                if(Equals(p.Object.Action.ToString(),"Encrypt"))
+                //if (p.Object.Action.HasFlag(ActionType.Encrypt))                                
+                {
+                    Console.WriteLine("we are inside encrypted block");
+                    string encryptedstring = StringCipher.Encrypt(p.Object.StringValue, password);
+                    Console.WriteLine("Encrypted value: " + encryptedstring);
+                }
+
+                if (Equals(p.Object.Action.ToString(),"Decrypt"))
+                {
+                    string decryptedstring = StringCipher.Decrypt(p.Object.StringValue, password);
+                    Console.WriteLine("Decrypted value: " + decryptedstring);
+                }
+                return;
             }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-            //Console.WriteLine("Please enter a password to use:");
-            //string password = Console.ReadLine();
-            //Console.WriteLine("Please enter a string to encrypt:");
-            //string plaintext = Console.ReadLine();
-            //Console.WriteLine("");
-//
-            //Console.WriteLine("Your encrypted string is:");
-            //string encryptedstring = StringCipher.Encrypt(plaintext, password);
-            //Console.WriteLine(encryptedstring);
-            //Console.WriteLine("");
-//
-            //Console.WriteLine("Your decrypted string is:");
-            //string decryptedstring = StringCipher.Decrypt(encryptedstring, password);
-            //Console.WriteLine(decryptedstring);
-            //Console.WriteLine("");
-//
-            //Console.WriteLine("Press any key to exit...");
-            //Console.ReadLine();
+            Console.WriteLine("Wrong parameters");
         }
     }
 }
